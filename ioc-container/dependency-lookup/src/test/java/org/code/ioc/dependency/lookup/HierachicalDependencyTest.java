1cd7f110f7e5ef59a032c105f9932014d2a59ceb
chenjunfeng02 <chenjunfeng02@meituan.com>
2022-11-16 19:20:37 +0800

    树形结构测试代码

---
+++ ioc-container/dependency-lookup/src/test/java/org/code/ioc/dependency/lookup/HierachicalDependencyTest.java
@@ -0,0 +1,13 @@
+package org.code.ioc.dependency.lookup;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HierachicalDependencyTest {
+
+    @Test
+    @DisplayName("hierachical dependency lookup")
+    void testHierachicalDependency() {
+
+    }
+}
